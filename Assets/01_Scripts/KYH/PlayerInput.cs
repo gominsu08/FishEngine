0c89b89b1396b4b5b1146f500f3b2f4c2a227ae8
이찬민 <a01076754042@gmail.com>
2024-07-15 23:30:17 +0900

    뭔지 모름

--- Assets/01_Scripts/KYH/PlayerInput.cs
+++ Assets/01_Scripts/KYH/PlayerInput.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    public Vector2 MoveDir { get; private set; }
+    public float jumpForce = 10;
 
+    public Vector2 MoveDir { get; private set; }
+    public Rigidbody2D rigid;
     public static bool stop;
-    public Action OnJump;
+    public bool isGround;
     public event Action OnFire;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private Vector2 attackRadius;
+
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
+        Jump();
+
         if (!stop)
             MoveInput();
+
+        GroundChecker();
+    }
+
+    private void GroundChecker()
+    {
+        isGround = Physics2D.OverlapBox(transform.position, attackRadius, whatIsGround); //하나 가져오기
     }
 
     private void MoveInput()
@@ -23,5 +45,19 @@ public class PlayerInput : MonoBehaviour
 
         MoveDir = new Vector2(x, 0);
         MoveDir = MoveDir.normalized;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rigid.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, attackRadius);
     }
 }
