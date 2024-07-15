c3f12c3ba86218fe20c258d703102eaeb5bd6e98
fishmaster <a01022639410@gmail.com>
2024-07-15 14:28:29 +0900

    feat : Lever

--- Assets/01_Scripts/KYH/Lever.cs
+++ Assets/01_Scripts/KYH/Lever.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
 
 public class Lever : MonoBehaviour , IInterection
 {
     private Animator animator;
     public UnityEvent leverOnEvent;
+    private SpriteRenderer inputBullun;//µµ¿ò ¸»Ç³¼±
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        inputBullun = GetComponentInChildren<SpriteRenderer>();
+        Debug.Log(inputBullun.name);
     }
     public void Interection()
     {
         leverOnEvent?.Invoke();
+        animator.SetBool("On", true);
+    }
+
+    public void InputHelp()
+    {
+        inputBullun.DOFade(1, 0.1f);
+    }
+    public void InputHelpOut()
+    {
+        inputBullun.DOFade(0, 0.1f);
     }
 }
