69d2d5213a8386685d4be449cb1969c2e6879a34
gominsu08 <minsu080618@gmail.com>
2024-07-16 00:53:45 +0900

    feat : UI

--- Assets/01_Scripts/Chanmin/Esc.cs
+++ Assets/01_Scripts/Chanmin/Esc.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,13 @@ public class Esc : MonoBehaviour
     public Button btb1;
     public Button btb2;
     public Image image;
+    public RectTransform rect;
 
     public void Restart()
     {
-        image.gameObject.SetActive(false);
+
+        rect.DOAnchorPosY(1378, 0.1f);
+
         Time.timeScale = 1;
     }
     public void Exit()
--- Assets/01_Scripts/Chanmin/EscManager.cs
+++ Assets/01_Scripts/Chanmin/EscManager.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class EscManager : MonoBehaviour
 {
     public Image image;
+
+    public RectTransform rect;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            image.gameObject.SetActive(true);
-            Time.timeScale = 0;
+            rect.DOAnchorPosY(-160, 0.1f).OnComplete(() => {
+                Time.timeScale = 0;
+            });
+
         }
     }
 }
