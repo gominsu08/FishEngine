ccf89addd2dbcb55ab1a7e61766b46fb5cf4c2b1
fishmaster <a01022639410@gmail.com>
2024-07-15 13:40:09 +0900

    feat : Light

--- Assets/01_Scripts/KYH/Lever.cs
+++ Assets/01_Scripts/KYH/Lever.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Lever : MonoBehaviour , IInterection
 {
     private Animator animator;
+    public UnityEvent leverOnEvent;
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
     public void Interection()
     {
-        //animator
+        leverOnEvent?.Invoke();
     }
 }
