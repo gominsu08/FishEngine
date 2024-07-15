3dba4bcb3d38bb78719cf10282a12e276d8cdf7f
fishmaster <a01022639410@gmail.com>
2024-07-15 15:41:34 +0900

    fix : teleport

--- Assets/TeleportStar.cs
+++ Assets/TeleportStar.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class TeleportStar : MonoBehaviour, IInterection
 {
-    [SerializeField]
     private Transform endTransform;
-    [SerializeField]
     private Transform startTransform;
-    [SerializeField]
     private Transform playerTransform;
     private int inPortal =0;
     public GameObject[] inputBullun;//µµ¿ò ¸»Ç³¼±
 
     private void Awake()
     {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+
+        startTransform = GetComponent<Transform>();
+        endTransform = GetComponentInChildren<Transform>();
+
         inputBullun[0].SetActive(false);
         inputBullun[1].SetActive(false);
     }
@@ -29,13 +31,13 @@ public class TeleportStar : MonoBehaviour, IInterection
         print(inPortal);
         if(inPortal == 0)
         {
-            playerTransform = endTransform;
+            playerTransform.position = endTransform.position;
             inPortal++;
             StartCoroutine(Wait());
         }
         else if(inPortal ==1)
         {
-            playerTransform = startTransform;
+            playerTransform.position = startTransform.position;
             inPortal--;
             StartCoroutine(Wait());
         }
